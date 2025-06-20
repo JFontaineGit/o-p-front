f3913a42595eccc017f1f0878b05935d76437994
Just Fontaine <itsmealeexvii@gmail.com>
2025-06-19 23:15:24 -0300

    feat: entorno de desarrollo

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+     production: false,
+     apiBaseUrl: 'http://127.0.0.1:8000',
+     wsBaseUrl: 'ws://127.0.0.1:8000',
+     apiTimeout: 10000,
+     retryAttempts: 3, 
+     retryDelay: 1000 
+   };
