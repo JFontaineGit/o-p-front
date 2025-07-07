3c5eea838597d22ef5b799174a36d3d5fcbd5ded
Just Fontaine <itsmealeexvii@gmail.com>
2025-07-07 19:00:57 -0300

    fix: env

--- src/app/environments/environment-prod.ts
+++ src/app/environments/environment-prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
-  apiBaseUrl: 'http://127.0.0.1:8080',
+  production: true,
+  apiBaseUrl: 'http://127.0.0.1:8080', 
   wsBaseUrl: 'ws://127.0.0.1:8080',
   apiTimeout: 10000,
   retryAttempts: 3,
