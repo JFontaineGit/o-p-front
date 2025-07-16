619f61e428388b6e3275a2b86ae43b05ba6f66ee
git-Joaquin-Gael <guzmanantonio867@gmail.com>
2025-07-16 20:29:55 -0300

    Update production environment URLs to use `olimpiadas-prog.onrender.com`.

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://127.0.0.1:8080', 
-  wsBaseUrl: 'ws://127.0.0.1:8080',
+  apiBaseUrl: 'https://olimpiadas-prog.onrender.com',
+  wsBaseUrl: 'ws://olimpiadas-prog.onrender.com',
   apiTimeout: 10000,
   retryAttempts: 3,
   retryDelay: 1000
